ae0d4555783599fae26a4db7d0fa0236ee2271ff
EinHerrSchmidt <matte24694@gmail.com>
2022-11-15 18:52:19 +0100

    Adapt size wrt screen width and height (rescaled). Added menu Open callback.

--- src/gui.py
+++ src/gui.py
@@ -1,20 +1,22 @@
 import sys
 import threading
 from tkinter import *
+from tkinter import filedialog
 from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import *
-from pandastable import Table
+from pandastable import Table, config
+import math
 
 from click import command
 
 
 class StdoutRedirector(object):
-    def __init__(self, textWidget):
-        self.textSpace = textWidget
+    def __init__(self, text_widget):
+        self.text_space = text_widget
 
     def write(self, string):
-        self.textSpace.insert("end", string)
-        self.textSpace.see("end")
+        self.text_space.insert("end", string)
+        self.text_space.see("end")
 
     def flush(self):
         pass
@@ -24,22 +26,28 @@ class GUI(object):
     def __init__(self, master):
         self.master = master
 
+        self.screen_width = self.master.winfo_screenwidth()
+        self.screen_height = self.master.winfo_screenheight()
+
+        print(self.screen_width)
+
         # notebooks and command panel
         self.upper_frame = Frame(master=self.master)
-        self.upper_frame.pack(side=TOP)
+        self.upper_frame.pack(side=TOP, fill=BOTH, expand=True)
 
         # log output
         self.lower_frame = Frame(master=self.master)
-        self.lower_frame.pack(side=BOTTOM)
+        self.lower_frame.pack(side=BOTTOM, fill=BOTH, expand=True)
 
         self.input_columns = 6
-        self.columns_translations = {"a": "Nome",
-                                     "b": "Cognome",
-                                     "c": "Prestazioni",
-                                     "d": "Anestesia",
-                                     "e": "Infezioni in atto",
-                                     "f": "Data inserimento in lista",
-                                     }
+        self.input_columns_translations = {"a": "Nome",
+                                           "b": "Cognome",
+                                           "c": "Prestazioni",
+                                           "d": "Anestesia",
+                                           "e": "Infezioni",
+                                           "f": "Data inserimento in lista",
+                                          }
+        self.output_columns = 10
 
         self.initializeUI()
 
@@ -49,14 +57,20 @@ class GUI(object):
         self.create_command_panel()
         self.create_log_text_box()
 
+        print("Welcome to the Interventional Radiology Planner and Scheduler.")
+
     def create_command_panel(self):
-        self.buttons_frame = Labelframe(master=self.upper_frame, text="", width=100)
-        self.buttons_frame.pack(side=RIGHT, fill=BOTH, anchor=E, padx=10)
+        self.buttons_frame = Labelframe(master=self.upper_frame, text="", width=math.floor(self.screen_width * 0.2))
+        self.buttons_frame.pack(side=RIGHT, expand=True, fill=BOTH, anchor=E)
 
         self.test_button = Button(master=self.buttons_frame,
                                   width=12,
                                   text="Test")
-        self.test_button.pack(padx=10)
+        self.test_button.pack()
+
+    def open_file_callback(self):
+        self.selected_file = filedialog.askopenfile()
+        print(self.selected_file.name)
 
     def create_upper_menus(self):
         menu = Menu(self.master)
@@ -68,15 +82,17 @@ class GUI(object):
         edit_menu = Menu(menu)
         menu.add_cascade(label="Edit", menu=edit_menu)
 
+        file_menu.add_command(label='Open...', command=self.open_file_callback)
+
     def create_notebooks(self):
         self.notebook_frame = Labelframe(self.upper_frame)
-        self.notebook_frame.pack(side=LEFT, fill=X, anchor=W, padx=10)
+        self.notebook_frame.pack(side=LEFT, fill=BOTH, anchor=W)
 
-        self.notebook = Notebook(self.notebook_frame)
-        self.notebook.pack(side=TOP, expand=True, fill=BOTH)
+        self.notebook = Notebook(self.notebook_frame, width=math.floor(self.screen_width * 0.8), height=math.floor(self.screen_height * 0.6))
+        self.notebook.pack(expand=True, fill=BOTH)
 
-        self.input_tab = Frame(self.notebook, width=900, height=500)
-        self.output_tab = Frame(self.notebook, width=900, height=500)
+        self.input_tab = Frame(self.notebook)
+        self.output_tab = Frame(self.notebook)
 
         self.input_tab.pack(fill=BOTH, expand=True)
         self.output_tab.pack(fill=BOTH, expand=True)
@@ -88,25 +104,26 @@ class GUI(object):
 
     def initialize_input_table(self):
         self.input_table = Table(parent=self.input_tab, cols=self.input_columns)
-        self.input_table.model.df = self.input_table.model.df.rename(columns=self.columns_translations)
+        self.input_table.model.df = self.input_table.model.df.rename(columns=self.input_columns_translations)
+
+        options={"cellwidth": 100}
+        config.apply_options(options,self.input_table)
+
+        self.input_table.columnwidths["Nome"] = 150
+        self.input_table.columnwidths["Prestazioni"] = 450
+        self.input_table.columnwidths["Data inserimento in lista"] = 250
         self.input_table.autoResizeColumns()
+        self.input_table.redraw()
+
         self.input_table.show()
 
     def create_log_text_box(self):
         self.output_frame = Frame(master=self.lower_frame)
         self.output_frame.pack(fill=BOTH)
 
-        # self.scroll_bar = Scrollbar(self.output_frame)
-        # self.scroll_bar.pack(side=RIGHT, fill=Y)
-
-        self.text_box = ScrolledText(
-            master=self.output_frame, width=200, height=16)
+        self.text_box = ScrolledText(master=self.output_frame, width=500, height=20)
         self.text_box.pack(fill=X)
         self.text_box.config(background="#000000", fg="#ffffff")
-        # to correctly resize the scroll bar
-        # self.text_box.config(yscrollcommand=self.scroll_bar.set)
-#
-        # self.scroll_bar.config(command=self.text_box.yview)
 
         sys.stdout = StdoutRedirector(self.text_box)
 

d43dc848d9649391fb681a7d6243fe0aaa285c63
EinHerrSchmidt <matte24694@gmail.com>
2022-11-25 20:46:57 +0100

    Progress.

--- src/bootstrap-table/bootstrap-table.py
+++ src/bootstrap-table/bootstrap-table.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk
-
+import tkinter.font as tkFont
 
 class Table(ctk.CTkFrame):
-    def __init__(self, master, rows, columns, header_height=30, row_height=20, column_width=150, row_separator_width=2, column_separator_width=1):
+    def __init__(self, master, rows, columns, header_height=30, row_height=20, column_width=150, row_separator_width=1, column_separator_width=1):
         super().__init__(master=master)
 
         self.horizontal_scrollbar = ctk.CTkScrollbar(master=self,
@@ -11,6 +11,7 @@ class Table(ctk.CTkFrame):
                                                    orientation=ctk.VERTICAL)
 
         self.rows = rows
+        self.columns = columns
         self.header_height = header_height
         self.header_color = "#ffffff"
         self.separator_line_color = "gray75"
@@ -18,6 +19,8 @@ class Table(ctk.CTkFrame):
         self.column_width = column_width
         self.even_row_col = "#f2f2f2"
         self.odd_row_col = "#ffffff"
+        self.cell_text_left_offset = 2 #px
+        self.font = ("Microsoft Tai Le", 14)
 
         self.row_separator_width = row_separator_width
         self.column_separator_width = column_separator_width
@@ -50,6 +53,7 @@ class Table(ctk.CTkFrame):
 
         self.draw_header()
         self.draw_table()
+        self.draw_text()
         self.bind_left_click()
  
     def draw_header(self):
@@ -70,7 +74,7 @@ class Table(ctk.CTkFrame):
 
     def draw_table(self):
 
-        y = 0  # self.header_height + 2 * self.row_separator_width + 1
+        y = 0
         for i in range(0, self.rows):
 
             self.table_canvas.create_rectangle(0, y,
@@ -92,8 +96,35 @@ class Table(ctk.CTkFrame):
 
         # TODO: draw columns
 
+    def draw_text(self):
+        
+        y = self.row_separator_width + self.row_height / 2
+        for row in range(0, self.rows):
+            x = self.cell_text_left_offset
+            for column in range(0, self.columns):
+                text = str((row, column)) + "asdasdasdasdasd"
+                max_displayable_text = self.compute_max_displayable(text)
+                self.table_canvas.create_text((x, y), text=max_displayable_text, font=self.font, anchor=ctk.W)
+                x = x + self.column_width
+            y = y + self.row_height + self.row_separator_width
+
+    def compute_max_displayable(self, text):
+        font = tkFont.Font(name=self.font[0], size=self.font[1])
+        text_width = font.measure(text)
+        text_height = font.metrics("linespace")
+
+        while text_width > self.column_width - self.cell_text_left_offset: 
+            text = text[:-1] # remove last char
+            text_width = font.measure(text)
+        return text
+        
+
+
     def bind_left_click(self):
         self.table_canvas.bind("<Button-1>", func=self.on_left_click)
+
+    def bind_vertical_scroll(self):
+        self.table_canvas.bind("<MouseWheel>", func=self.table_canvas.yview, add="+")
 
     # get cell
     def on_left_click(self, event):
@@ -101,13 +132,14 @@ class Table(ctk.CTkFrame):
         column = 0
 
         vertical_scrollbar_offset = self.table_canvas.winfo_reqheight() * self.vertical_scrollbar.get()[0]
+        horizontal_scrollbar_offset = self.table_canvas.winfo_reqwidth() * self.horizontal_scrollbar.get()[0]
 
-        y = 0 # self.header_height + 2 * self.row_separator_width
+        y = 0
         x = 0
         while y < event.y + vertical_scrollbar_offset:
             y += self.row_height + self.row_separator_width
             row += 1
-        while x < event.x:
+        while x < event.x + horizontal_scrollbar_offset:
             x += self.column_width
             column += 1
 
@@ -129,7 +161,7 @@ class Table(ctk.CTkFrame):
 root = ctk.CTk()
 root.title("Fancy table")
 
-table = Table(master=root, rows=60, columns=6, row_height=20, header_height=40)
+table = Table(master=root, rows=600, columns=9, row_height=70, header_height=80)
 table.pack(expand=True, fill=ctk.BOTH)
 
 root.mainloop()
